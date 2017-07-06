8104b74b9d82bd831ca562d8e4461330dfe6bbb7
Pierre <pvrooyen@gmail.com>
2017-07-06 12:07:48 +0200

    add pair Requester & Responder + tests

--- test/Network/mempool.js
+++ test/Network/mempool.js
@@ -6,22 +6,22 @@ const Mempool = require('../../lib/mempool/mempool');
 describe('Network - Mempool', function() {
     let mempool = new Mempool()
 
-    it('should open the mempool',function () {
-        mempool.open();
+    it('should open the mempool', function() {
+        // mempool.open();
     });
-    it('should handle incomming relevant object',function () {
-        
+    it('should handle incomming relevant object', function() {
+
     });
-    it('should verify invalid received object',function () {
-        
+    it('should verify invalid received object', function() {
+
     });
-    it('should handle duplicate received data',function () {
-        
+    it('should handle duplicate received data', function() {
+
     });
-    it('should be able to retrieve a specific data',function () {
-        
+    it('should be able to retrieve a specific data', function() {
+
     });
-    it('should destroy the mempool',function () {
-        mempool.close();
+    it('should destroy the mempool', function() {
+        // mempool.close();
     });
 });
