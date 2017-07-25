d15529046de93bd7628ab2dde06afe06aa2984fe
Pierre <pvrooyen@gmail.com>
2017-07-25 16:15:22 +0200

    validate mn synced mempool data

--- test/Network/mempool.js
+++ test/Network/mempool.js
@@ -3,6 +3,20 @@ const should = require('should');
 const assert = require('assert');
 const Node = require('../../lib/node/node');
 const mocks = require('../../lib/mocks/mocks');
+// const bld = require('bitcore-lib-dash')
+
+// var mns = []
+// for (let i = 0; i < 10; i++) {
+//     var privateKey = new bld.PrivateKey()
+
+//     mns.push({
+//         id: `MasterNode_${i}`,
+//         privKey: privateKey.toString(),
+//         publicAdr: privateKey.publicKey.toAddress().toString()
+//     })
+// }
+
+// console.log(mns)
 
 describe('Network - Mempool', function() {
 
@@ -11,22 +25,56 @@ describe('Network - Mempool', function() {
         done()
     })
 
-    it('should sync a value on the list of masternodes', function(done) {
-        const key = 'testKey';
+    it('should sync a value from a VALID masternode on the list of masternodes', function(done) {
+        const key = 'mn_valid_sync';
         const value = new Date().getTime();
-        var parms = {}; //use default values
         var nodes = [];
 
         mocks.mnList.map(mn => {
+            let parms = {
+                pubKey: mn.publicAdr,
+                privKey: mn.privKey
+            }
             nodes.push(new Node(parms));
         })
 
-        nodes[0].addMemPoolData(value, key, 'validPK', 'validSignature');
+        nodes[0].addMemPoolData(nodes[0].config.privKey, nodes[0].config.pubKey, value, key);
 
         setTimeout(() => {
+
             nodes.filter(n => {
-                return n.getMemPoolData(key) == value;
+                let data = n.getMemPoolData(key)
+                return data && data.value == value;
             }).length.should.equal(mocks.mnList.length);
+            done();
+
+        }, 1000)
+    })
+
+    it('should sync a value from a VALID masternode on the list of masternodes', function(done) {
+        const key = 'mn_invalid_sync';
+        const value = new Date().getTime();
+        var nodes = [];
+
+        mocks.mnList.map(mn => {
+            let parms = {
+                pubKey: mn.publicAdr,
+                privKey: mn.privKey
+            }
+            nodes.push(new Node(parms));
+        })
+
+        //change MN privkey to valid key but not in the mnList to simulate invalid MN
+        nodes[0].config.privKey = 'ce0e2e1b39cef330e8d645ddec8724f737f2f44b7c9f4f78dc3b33d62de003cd'
+
+        nodes[0].addMemPoolData(nodes[0].config.privKey, nodes[0].config.pubKey, value, key);
+
+        setTimeout(() => {
+
+            nodes.filter(n => {
+                let data = n.getMemPoolData(key)
+                return data && data.value == value
+            }).length.should.equal(0);
             done();
 
         }, 1000)
