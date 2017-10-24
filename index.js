de4f86c862ab1fc92202e9d708eefdf4f42705bd
Pierre <pvrooyen@gmail.com>
2017-10-24 09:17:41 +0200

    test enhancement - simulate multiple nodes localhost

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const Dapi = require('./lib/dapi');
-let dapi = new Dapi(require('./dapi.json'));
+//let dapi = new Dapi(require('./dapi.json'));
+
+//QDEVTEMP
+let dapiCount = 10; //start 10 dapi instances
+let dapiArr = [];
+
+let dapiBuilder = function() {
+    dapiArr.push(new Dapi(require('./dapi.json')))
+    if (dapiArr.length >= dapiCount) {
+        clearInterval(dapiInt);
+    }
+}
+
+let dapiInt = setInterval(dapiBuilder, 500);
+//QDEVTEMP END
+
+
+
