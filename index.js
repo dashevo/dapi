668b5c01955156e970008518aefa55d75d557d7d
Pierre <pvrooyen@gmail.com>
2017-11-06 10:11:43 +0200

    refactor (oprimise architecture)

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const Dapi = require('./lib/dapi');
 //let dapi = new Dapi(require('./dapi.json'));
 
 //QDEVTEMP
-let dapiCount = 1; //start 10 dapi instances
 let dapiArr = [];
+let nonQuorumFactor = 10;
 
-
-for (let i = 0; i < dapiCount; i++) {
+for (let i = 0; i < require('quorums-dash').quorumSize * nonQuorumFactor; i++) {
     let config = require('./dapi.json');
     config.server.port = 3000 + i;
     dapiArr.push(new Dapi(config))
 }
-
-
-
-
 //QDEVTEMP END
 
 
