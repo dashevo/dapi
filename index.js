c0d4d62320f04d291c21615d466d4049bbe06e67
Pierre <pvrooyen@gmail.com>
2017-11-07 10:46:30 +0200

    config improvents

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Dapi = require('./lib/dapi');
 //let dapi = new Dapi(require('./dapi.json'));
+const qDash = require('quorums-dash')
 
 //QDEVTEMP
 let dapiArr = [];
-let nonQuorumFactor = 10;
 
-for (let i = 0; i < require('quorums-dash').quorumSize * nonQuorumFactor; i++) {
+for (let i = 0; i < qDash.config.quorumSize * qDash.config.dapiMultiplicator; i++) {
     let config = require('./dapi.json');
     config.server.port = 3000 + i;
     dapiArr.push(new Dapi(config))
