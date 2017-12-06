58926de8f25bf93d01f10eacd7b82828a2704e0e
Pierre <pierre@dash.org>
2017-12-06 10:30:50 +0200

    fix authservice tests

--- test/authentication/authService.js
+++ test/authentication/authService.js
@@ -1,5 +1,4 @@
-const authServiceLib = require('../../lib/authService/authService');
-const should = require('should');
+require('../_before')
 
 const Mnemonic = require('bitcore-mnemonic-dash');
 const Message = require('bitcore-message-dash');
@@ -7,19 +6,16 @@ let mnemonic = new Mnemonic('jaguar paddle monitor scrub stage believe odor frow
 let privKey = mnemonic.toHDPrivateKey().derive("m/1/1495176227").privateKey;
 let txId = 'cb1aa5d405c148a4990ff0035a6cd86cc73857ea57be3e49539cd8a9d0358315';
 
-
 describe('AuthService ', function() {
 
-    let authService = new authServiceLib();
-
-    let challenge = authService.getChallenge();
+    let challenge = dapi.authService.getChallenge();
     it('should get a challenge string', function() {
         challenge.should.exist;
     });
 
     it('should resolve a valid signature for challenge', function() {
         let nonce = 2;
-        return authService.resolveChallenge('pierre', nonce, new Message(nonce.toString()).sign(privKey))
+        return dapi.authService.resolveChallenge('pierre', nonce, new Message(nonce.toString()).sign(privKey))
             .then(res => {
                 res.should.be.true
             })
@@ -27,7 +23,7 @@ describe('AuthService ', function() {
 
     it('it should fail for invalid nonce', function() {
         let nonce = 0;
-        return authService.resolveChallenge('pierre', nonce, new Message(nonce.toString()).sign(privKey))
+        return dapi.authService.resolveChallenge('pierre', nonce, new Message(nonce.toString()).sign(privKey))
             .then(res => {
                 res.should.be.false
             })
