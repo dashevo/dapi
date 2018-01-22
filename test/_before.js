e7d4d2f21b8b81eb839c0fd52905605fd68cc28f
Kamuela Franco <kamuela.franco@gmail.com>
2018-01-22 12:33:17 +0000

    [EV-593] Troubleshooting build

--- test/_before.js
+++ test/_before.js
@@ -1,7 +1,7 @@
 // TODO: Address ESLint issues the next time this file is edited
 /* eslint-disable */
 if (!global.hasOwnProperty('dapi')) {
-  const Dapi = require('../lib/dapi.js');
-  global.dapi = new Dapi(require('../lib/config'));
+  //const Dapi = require('../lib/dapi.js');
+  //npglobal.dapi = new Dapi(require('../lib/config'));
   const should = require('should');
 }
