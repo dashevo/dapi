648d1ee580349fb21cde75d1cceadb7cc7aef7a3
Pierre <pierre@dash.org>
2018-02-08 07:59:58 +0200

    revert interrim commit

--- test/rpc/rpc.js
+++
@@ -1,9 +0,0 @@
-let rpcComm = require('../../lib/rpc')
-
-it('should get final balance', (done) => {
-    return rpcComm.getBalance(['ydo8pdZQKuVGjoRwAg4ngZ4sSh67zMizYA'], function (err, res) {
-        console.log(res.result)
-    })
-    done();
-})
-
