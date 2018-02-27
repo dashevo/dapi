5d629ba54ea1efddcbd16c2f6d6d13955191494f
Cofresi <cofresi@dash.org>
2018-02-27 17:31:39 -0400

    Merge branch 'feature/single_entry_point' into dkg
    #	index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const Dapi = require('./lib/dapi');
-const config = require('./lib/config');
-const qDash = require('quorums-dash');
-
-//const dapi = new Dapi(config);
-
-// QDEVTEMP
-const dapiArr = [];
-
-for (let i = 0; i < qDash.config.quorumSize * qDash.config.dapiMultiplicator; i += 1) {
-  const newConfig = Object.assign({}, config);
-  newConfig.server.port = 3000 + i;
-  dapiArr.push(new Dapi(newConfig));
-}
-// QDEVTEMP END
-
+require('./lib/app');
