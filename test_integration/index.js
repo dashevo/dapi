acd125bbe4dd63f74942cdd13d0884544e04e84c
Pierre <pierre@dash.org>
2018-04-19 10:51:08 +0200

    fix integration test

--- test_integration/index.js
+++ test_integration/index.js
@@ -1,13 +1,10 @@
 const SpvService = require('../lib/services/spv');
-const config = require('../lib/config');
 const bitcore = require('bitcore-lib-dash');
 
 bitcore.Networks.defaultNetwork = bitcore.Networks.testnet;
 
 const log = require('../lib/log');
 const bloomFilter = require('bloom-filter');
-
-const spvService = new SpvService(config.dashcore.p2p);
 
 const listenToBloomFilter = (privKey) => {
   const nbElements = 100;
@@ -17,7 +14,7 @@ const listenToBloomFilter = (privKey) => {
   const publicKey = privKey.toPublicKey();
   filter.insert(bitcore.crypto.Hash.sha256ripemd160(publicKey.toBuffer()));
 
-  spvService
+  SpvService
     .loadBloomFilter(filter)
     .then(() => {
       log.info('...filtering for transactions & block on', publicKey.toAddress().toString('hex'));
@@ -32,7 +29,7 @@ const listenToBloomFilter = (privKey) => {
       //   .getBlocks('0000000000747fd6af8c8aa61da1c2ec1f089fafc824bae9c3bf7ef51f20a777');
 
       setInterval(() => {
-        const clientCache = spvService.getData(filter);
+        const clientCache = SpvService.getData(filter);
         if (clientCache && clientCache.merkleblocks.length > lastMerkleLength) {
           log.info(`MOCK CLIENT ${publicKey.toAddress().toString('hex')}: ${clientCache.merkleblocks.length - lastMerkleLength} new merkle block(s) found`);
           lastMerkleLength = clientCache.merkleblocks.length;
