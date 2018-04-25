fd4da752e24120b72f311e9bd7b4796a27076c2d
Anton <antouhou@gmail.com>
2018-04-25 16:47:52 +0300

    [EV-767] refactored getBlocks command and getHistoricBlockchainDataSyncStatus commands

--- test/fixtures/coreAPIFixture.js
+++ test/fixtures/coreAPIFixture.js
@@ -7,4 +7,8 @@ module.exports = {
   async getBalance(address) { return 100; },
   async getBestBlockHeight() { return 243789; },
   async getBlockHash() { return 'hash'; },
+  async getBlocks(limit, blockDate) { return [{}]; },
+  async getHistoricBlockchainDataSyncStatus() {
+    return {};
+  },
 };
