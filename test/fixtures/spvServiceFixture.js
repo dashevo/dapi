3e6a4fabbce03c51402342189ec123e966855831
Pierre <pierre@dash.org>
2018-06-11 13:19:24 +0200

    clearBloomFilter refactor + tests

--- test/fixtures/spvServiceFixture.js
+++ test/fixtures/spvServiceFixture.js
@@ -1,4 +1,5 @@
 module.exports = {
   async loadBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
   async addToBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
+  async clearBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
 };
