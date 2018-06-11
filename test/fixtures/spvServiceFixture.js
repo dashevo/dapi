a0bfcd9392d7d99ab0ad2ead53aff6e444bffd83
Pierre <pierre@dash.org>
2018-06-11 13:40:40 +0200

    getSpvData refactor + tests

--- test/fixtures/spvServiceFixture.js
+++ test/fixtures/spvServiceFixture.js
@@ -2,4 +2,5 @@ module.exports = {
   async loadBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
   async addToBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
   async clearBloomFilter(filter) { return new Promise((resolve, reject) => { resolve(true); }); },
+  async getSpvData(filter) { return { transactions: [], merkleblocks: [] }; },
 };
