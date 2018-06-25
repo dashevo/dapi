7205f916b7da02b213e71f174f0c1c59489518d0
ABVgedeika <abvgedeika@gmail.com>
2018-06-25 23:34:17 +0300

    add test/services/mempool/KeyValueStore.js

--- test/services/KeyValueStore.js
+++
@@ -1,6 +0,0 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-
-const KeyValueStore = require('../../lib/services/mempool/KeyValueStore');
-
-describe('KeyValueStore', () => {});
