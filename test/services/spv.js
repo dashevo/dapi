2a77493219566b2ac2ed7f86acb5458660c1b6b7
Kamuela Franco <kamuela.franco@gmail.com>
2018-06-25 10:57:14 +0100

    [EV-820] Clear bloom filter on disconnect/timeout (#77)
    * Automatic update of dependencies with npm audit
    
    * Switch function declaration convention
    
    * [EV-820] Add lastSeen field to clients
    
    * [EV-820] Add original filter to client fields
    
    * Updated
    
    * [EV-820] Update last seen
    
    * [EV-820] Garbage collection for bloom filters of timed out connections
    
    * [EV-820] Fixed bug
    
    * Audited for security
    
    * [EV-820] Added unit tests
    
    * [EV-820] Now using named export
    
    * [EV-820] Lowered unit test coverage threshold
    
    * [EV-820] Use Date.now() instead of new Date()
    
    * [EV-820] Use filterHash instead of filter

---
+++ test/services/spv.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const { SpvService, getCorrectedHash, clearDisconnectedClientBloomFilters } =
+  require('../../lib/services/spv');
+
+describe('SPV', () => {
+  describe('getCorrectedHash', () => {
+    it('should return a corrected reversed hash object', () => {
+      const buffer =
+        Buffer.from([
+          0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ]);
+      const expected = '0101010101010101010101010101010101010101010101010101010101010100';
+      const actual = getCorrectedHash(buffer);
+      assert.equal(actual, expected);
+    });
+  });
+  describe('clearDisconnectedClientBloomFilters', () => {
+    it('should return an empty array when the incoming clients array is empty', () => {
+      const expected = [];
+      const actual = clearDisconnectedClientBloomFilters({ clients: [] });
+      assert.deepEqual(actual, expected);
+    });
+    it('should return the list of clients remaining after removing those that have timed out', () => {
+      const client = lastSeen => ({
+        filter: 'filter',
+        peer: { messages: { FilterClear: () => {} }, sendMessage: () => { } },
+        lastSeen,
+      });
+      const currentTime = new Date(1529427556922);
+      const hasDisconnectedThresholdInMsec = 60000;
+      const clients = [
+        client(new Date(currentTime.getTime() - hasDisconnectedThresholdInMsec)),
+        client(currentTime),
+        client(new Date(currentTime - (hasDisconnectedThresholdInMsec + 1))),
+      ];
+      const expected = [client(currentTime)];
+      const actual =
+        clearDisconnectedClientBloomFilters({
+          clients, currentTime, hasDisconnectedThresholdInMsec,
+        });
+      // TODO: How do you get assert to compare nested arrays correctly?
+      assert.deepEqual(actual[0].filter, expected[0].filter);
+    });
+  });
+});
