15a6b134750d8461ead38ba35ae398c7a8b713a7
Pierre <pierre@dash.org>
2018-08-08 12:50:50 +0200

    add tests

--- test/rpcServer/commands/getMnListDiff.js
+++ test/rpcServer/commands/getMnListDiff.js
@@ -1,22 +1,48 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const getMNListDiffFactory = require('../../../lib/rpcServer/commands/getMnListDiff');
 const coreAPIFixture = require('../../fixtures/coreAPIFixture');
-const getMnListDiffFactory = require('../../../lib/rpcServer/commands/getMnListDiff');
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
+let spy;
 
-const coreAPI = require('../../../lib/app');
-
-describe('getMnListDiff', () => {
+describe('getMNListDiff', () => {
   describe('#factory', () => {
     it('should return a function', () => {
-      const getBalance = getMnListDiffFactory(coreAPI);
-      expect(getBalance).to.be.a('function');
+      const getMNListDiff = getMNListDiffFactory(coreAPIFixture);
+      expect(getMNListDiff).to.be.a('function');
     });
+  });
 
-    it('Should return a number', async () => {
-      const diff = getMnListDiffFactory(coreAPI);
-    });
+  before(() => {
+    spy = sinon.spy(coreAPIFixture, 'getMnListDiff');
+  });
+
+  beforeEach(() => {
+    spy.resetHistory();
+  });
+
+  after(() => {
+    spy.restore();
+  });
+
+  it('Should return a masternode list difference list object', async () => {
+    const getMNListDiff = getMNListDiffFactory(coreAPIFixture);
+    expect(spy.callCount).to.be.equal(0);
+
+    const baseBlockHash = '0000000000000000000000000000000000000000000000000000000000000000';
+    const blockHash = '0000000000000000000000000000000000000000000000000000000000000000';
+
+    const mnDiffList = await getMNListDiff({ baseBlockHash, blockHash });
+    expect(mnDiffList).to.be.an('object');
+    expect(mnDiffList.baseBlockHash.length).to.equal(64);
+    expect(mnDiffList.blockHash.length).to.equal(64);
+    expect(mnDiffList.merkleRootMNList.length).to.equal(64);
+    expect(mnDiffList.deletedMNs).to.be.an('Array');
+    expect(mnDiffList.mnList).to.be.an('Array');
+
+    expect(spy.callCount).to.be.equal(1);
   });
 });
