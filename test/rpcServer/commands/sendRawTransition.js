7dfaff940695267733a93223690d26cae5d412c7
Kamuela Franco <kamuela.franco@gmail.com>
2018-09-04 18:13:22 +0100

    [EV-999] Updated to dash-schema v3

--- test/rpcServer/commands/sendRawTransition.js
+++ test/rpcServer/commands/sendRawTransition.js
@@ -1,34 +1,58 @@
+/* eslint-disable no-underscore-dangle */
 const chai = require('chai');
-const Schema = require('@dashevo/dash-schema');
+const crypto = require('crypto');
+const Schema = require('@dashevo/dash-schema/dash-schema-lib');
 const { PrivateKey, Transaction } = require('@dashevo/dashcore-lib');
+
+const { createStateTransition } = require('../../../lib/rpcServer/commands/sendRawTransition');
 
 const { expect } = chai;
 
-const { createStateTransition } = require('../../../lib/rpcServer/commands/sendRawTransition');
+const hash = crypto.createHash('sha256');
 
 describe('sendRawTransition', () => {
   describe('#createStateTransition', () => {
     it('should throw an error when no stateTransitionDataPacket given as an argument');
     it('should throw an error when the hash of the data packet does not match the hash in header');
     it('should return a stateTransition', () => {
-      const rawTransitionHeader = '03000c00000000000000ac01003c0a168a4d512742516a80a94293ad86ab2cb547415e8b96719a89f91048dfd03c0a168a4d512742516a80a94293ad86ab2cb547415e8b96719a89f91048dfd0e8030000000000003a0a168a4d512742516a80a94293ad86ab2cb547415e8b96719a89f91048dfa0411f3ae683b0a3ac3c3342ab30e646df344e8c3648902b48c5cb5f29c17f15a43ad93943b49c1f83a06321c6c434ae1c73d22ae83da3d39b9c5ce98a7947f5deab90';
+      let rawTransitionHeader = '03000c00000000000000ac01003c0a168a4d512742516a80a94293ad86ab2cb547415e8b96719a89f91048dfd03c0a168a4d512742516a80a94293ad86ab2cb547415e8b96719a89f91048dfd0e803000000000000f10b1c3217f0982a76623ae2639305f6ad788afbfedc89b584bbcd10f8a912c3411f55df779a07a9e395413bab34a97d003bf185e7f5d6116c5a9fd8a7fee582c7f076aa48f44902740e2784cd18adf4478374f25804d082f2ae8b886425742af1d4';
+      const transitionDataPacket = {
+        stpacket: {
+          pver: 1,
+          dapid: 'af462ee93b79b6991ebdc569f84c681c77525ad679d1c8b01087dbbbfbb3658d',
+          dapcontract: {
+            pver: 1,
+            idx: 0,
+            dapschema: {},
+            dapver: 1,
+            dapname: 'dapname',
+            meta: {
+              id: 'af462ee93b79b6991ebdc569f84c681c77525ad679d1c8b01087dbbbfbb3658d',
+            },
+          },
+        },
+      };
+
+      const rawTransitionDataPacket = Schema.serialize.encode(transitionDataPacket).toString('hex');
+      const packetHash = hash.update(rawTransitionDataPacket).digest('hex');
       const headerTransaction = new Transaction(rawTransitionHeader);
-      const packet = Schema.create.tspacket();
-      const rawTransitionDataPacket = packet;
-      // eslint-disable-next-line no-underscore-dangle
-      const packetHash = Schema.hash.tspacket(packet);
+
       headerTransaction.extraPayload.setHashSTPacket(packetHash);
       const privateKey = new PrivateKey();
       headerTransaction.extraPayload.sign(privateKey);
 
+      rawTransitionHeader = headerTransaction.serialize();
+
       const expected = {
         headerTransaction,
-        packet,
+        packet: transitionDataPacket,
       };
+      expected.headerTransaction._inputAmount = undefined;
+      expected.headerTransaction._outputAmount = undefined;
 
-      const actual =
-        createStateTransition({ rawTransitionHeader, rawTransitionDataPacket });
-      expect(actual).to.be.deepEqual(expected);
+      const actual = createStateTransition({ rawTransitionHeader, rawTransitionDataPacket });
+
+      expect(actual).to.be.deep.equal(expected);
     });
   });
 });
