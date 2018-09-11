6d4f188f4ef1d7065e847ca15ca12c1ebe0918f6
Anton Suprunchuk <antouhou@gmail.com>
2018-09-11 15:02:26 +0300

    [EV-1053] Fixed eslint errors

--- test/fixtures/dashDriveFixture.js
+++ test/fixtures/dashDriveFixture.js
@@ -1,3 +1,4 @@
+/* eslint class-methods-use-this: off */
 const AbstractDashDriveAdapter = require('../../lib/api/dashDriveAdapter/AbstractDashDriveAdapter');
 
 // Create a class, so JSDoc would work properly in our tests
