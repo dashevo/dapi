fce68a4bfb02613755d66f8e60a9623b5ff457e5
Anton Suprunchuk <antouhou@gmail.com>
2018-09-11 15:59:32 +0300

    [EV-1053] Feature/dd adapter (#96)
    * [EV-1053] Add base interface for the DD adapter
    
    * [EV-1053] Add abstract class for DD adapter
    
    * [EV-1053] Disabled eslint unused-vars and this-in-methods rules for the abstract DD adapter class
    
    * [EV-1053] Add jayson client to DD adapter
    
    * [EV-1053] Add requesting DD methods in adapter
    
    * [EV-1053] Add tests for the DD adapter
    
    * [EV-1053] Add creation of DD adapter to the DAPI enrtypoint, add dashdrive host and port to the DAPI config
    
    * [EV-1053] Add new DashDrive adapter to the sendRawTransition method
    
    * [EV-1053] Rename getDapContract to fetchDapContract, make fetchDapContract method to use new DD Adapter
    
    * [EV-1053] Create new fixture for DD adapter and fixed old tests
    
    * [EV-1053] Fixed eslint errors

--- test/fixtures/dashDriveFixture.js
+++ test/fixtures/dashDriveFixture.js
@@ -1,7 +1,10 @@
-module.exports = {
-  async pinPacket(header, packet) { return 'tsid'; },
-  async getDapSpace(dapId, userNameOrId) { return {}; },
-  async getDapContext(dapId, usernameOrId) { return {}; },
-  async getDapContract(dapId) { return {}; },
-  async searchDapContracts(pattern) { return []; },
-};
+/* eslint class-methods-use-this: off */
+const AbstractDashDriveAdapter = require('../../lib/api/dashDriveAdapter/AbstractDashDriveAdapter');
+
+// Create a class, so JSDoc would work properly in our tests
+class DashDriveFixture extends AbstractDashDriveAdapter {
+  addSTPacket(packet) { return Promise.resolve('tsid'); }
+  fetchDapContract(dapId) { return Promise.resolve({}); }
+}
+
+module.exports = new DashDriveFixture();
