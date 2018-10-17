5f6edfa978985b29758195b216a980e96cc4927c
Anton Suprunchuk <antouhou@gmail.com>
2018-10-17 15:16:40 +0100

    Remove infinitely pending tests

--- test/integration/insight/insight.js
+++ test/integration/insight/insight.js
@@ -12,23 +12,23 @@ const config = require('../../../lib/config/index');
 const URI = config.insightUri;
 
 xdescribe('Insight - Integration', () => { // 5975
-  describe('#getAddress(txHash)', () => {
-    const txHash = '50622f66236671501c0e80f388d6cf1e81158de8526f4acc9db00adf3c524077';
-    it('should return address', () => insight.getTransactionFirstInputAddress(txHash).then((address) => {
-      expect(address).to.be.a('string');
-    }));
-  });
-
-  describe('#getCurrentBlockHeight', () => {
-    it('should return address', () => insight.getCurrentBlockHeight().then((blockHeight) => {
-      expect(blockHeight).to.be.a('number');
-    }));
-  });
-
-  describe('#getBestBlockHeight', () => {
-    const txHash = '50622f66236671501c0e80f388d6cf1e81158de8526f4acc9db00adf3c524077';
-    it('should return address', () => insight.getBestBlockHeight().then((blockHeight) => {
-      expect(blockHeight).to.be.a('number');
-    }));
-  });
+  // describe('#getAddress(txHash)', () => {
+  //   const txHash = '50622f66236671501c0e80f388d6cf1e81158de8526f4acc9db00adf3c524077';
+  //   it('should return address', () => insight.getTransactionFirstInputAddress(txHash).then((address) => {
+  //     expect(address).to.be.a('string');
+  //   }));
+  // });
+  //
+  // describe('#getCurrentBlockHeight', () => {
+  //   it('should return address', () => insight.getCurrentBlockHeight().then((blockHeight) => {
+  //     expect(blockHeight).to.be.a('number');
+  //   }));
+  // });
+  //
+  // describe('#getBestBlockHeight', () => {
+  //   const txHash = '50622f66236671501c0e80f388d6cf1e81158de8526f4acc9db00adf3c524077';
+  //   it('should return address', () => insight.getBestBlockHeight().then((blockHeight) => {
+  //     expect(blockHeight).to.be.a('number');
+  //   }));
+  // });
 });
