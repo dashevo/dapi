a419c03d537ce0535ce4e3a36c19f6ed90655a2b
Anton Suprunchuk <antouhou@gmail.com>
2018-11-29 13:58:35 +0300

    Fix generate command behaviour when running in devnet mode; Add `isDevnet` function to utils; Move `isRegtest` function from config ti utils; Covered both functions with unit tests

---
+++ test/utils/index.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+
+const utils = require('../../lib/utils');
+
+const { expect } = chai;
+
+describe('utils', () => {
+  describe('#isRegtest', () => {
+    it('Should return true only if "regtest" string passed', () => {
+      expect(utils.isRegtest('regtest')).to.be.true;
+
+      expect(utils.isRegtest('regtest=')).to.be.false;
+      expect(utils.isRegtest('_regtest')).to.be.false;
+      expect(utils.isRegtest('retest')).to.be.false;
+      expect(utils.isRegtest('devnet')).to.be.false;
+      expect(utils.isRegtest('mainnet')).to.be.false;
+      expect(utils.isRegtest('testnet')).to.be.false;
+    });
+  });
+  describe('#isDevnet', () => {
+    it('Should return true only if string that starts from "devnet" passed', () => {
+      expect(utils.isDevnet('devnet')).to.be.true;
+      expect(utils.isDevnet('devnet=mysuperdevnet')).to.be.true;
+
+      expect(utils.isDevnet('_devnet')).to.be.false;
+      expect(utils.isDevnet('regtest=')).to.be.false;
+      expect(utils.isDevnet('_regtest')).to.be.false;
+      expect(utils.isDevnet('retest')).to.be.false;
+      expect(utils.isDevnet('mainnet')).to.be.false;
+      expect(utils.isDevnet('testnet')).to.be.false;
+    });
+  });
+});
