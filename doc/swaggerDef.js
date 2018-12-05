d27e821b536973fdac68cf481ac2833f2bb83906
thephez <thephez@users.noreply.github.com>
2018-12-05 17:21:58 -0500

    Lint fixes

--- doc/swaggerDef.js
+++ doc/swaggerDef.js
@@ -9,8 +9,8 @@
 */
 
 module.exports = {
-  "openapi": "3.0.0",
-  "x-api-id": "dapi-devnet",
+  openapi: '3.0.0',
+  'x-api-id': 'dapi-devnet',
   info: {
     // API informations (required)
     title: 'DAPI devnet',
@@ -19,37 +19,37 @@ module.exports = {
   },
   servers: [
     {
-    "url": "http://devnet-maithai.thephez.com:3000"
+      url: 'http://devnet-maithai.thephez.com:3000',
     },
   ],
-  //basePath: '/', // Base path (optional)
-  "paths": {},
-  "components": {
-    "schemas": {
-      "JsonRpcRequired": {
-        "type": "object",
-        "required": [
-          "id",
-          "jsonrpc"
+
+  paths: {},
+  components: {
+    schemas: {
+      JsonRpcRequired: {
+        type: 'object',
+        required: [
+          'id',
+          'jsonrpc',
         ],
-        "properties": {
-          "id": {
-            "type": "integer",
-            "default": 1,
-            "format": "int32",
-            "description": "Request ID"
+        properties: {
+          id: {
+            type: 'integer',
+            default: 1,
+            format: 'int32',
+            description: 'Request ID',
           },
-          "jsonrpc": {
-            "type": "string",
-            "default": "2.0",
-            "description": "JSON-RPC Version (2.0)"
+          jsonrpc: {
+            type: 'string',
+            default: '2.0',
+            description: 'JSON-RPC Version (2.0)',
           }
         },
-        "discriminator": {
-          "propertyName": "method_name"
-        }
-      }
-    }
+        discriminator: {
+          propertyName: 'method_name',
+        },
+      },
+    },
   },
   /** Readme.io swagger extensions
   * ------------------------------
@@ -58,40 +58,40 @@ module.exports = {
   * - Whether to send the defaults specified in your swagger file, or render
   *   them as placeholders
   */
-  "x-send-defaults": true,
+  'x-send-defaults': true,
   /**
   * x-headers (default: undefined)
   * - Array of static headers to add to each request. Must be provided as an
   *   array of JSON objects with `key` and `value` properties.
   */
-  "x-headers": [],
+  'x-headers': [],
   /**
   * x-explorer-enabled (default: true)
   * - Enable the API explorer
   */
-  "x-explorer-enabled": true,
+  'x-explorer-enabled': true,
   /**
   * x-proxy-enabled (default: true)
   * - Whether the Readme CORs proxy is enabled or not. If your API correctly
   *   returns CORs headers, you can safely turn this off.
   */
-  "x-proxy-enabled": true,
+  'x-proxy-enabled': true,
   /**
   * x-samples-enabled (default: true)
   * - Enable code examples
   */
-  "x-samples-enabled": true,
+  'x-samples-enabled': true,
   /**
   * x-samples-language
   * - Languages to generate code samples for
   * Default: ['curl', 'node', 'ruby', 'javascript', 'python']
   * Supported: node, curl, ruby, javascript, objectivec, python, java, php, csharp, swift, go
   */
-  "x-samples-languages": [
-    "curl",
-    "node",
-    "ruby",
-    "javascript",
-    "python"
-  ]
+  'x-samples-languages': [
+    'curl',
+    'node',
+    'ruby',
+    'javascript',
+    'python',
+  ],
 };
