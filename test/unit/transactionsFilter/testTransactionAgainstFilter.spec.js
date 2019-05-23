aec3e6871905e4ae5cfb475588af79ef25671c4e
Anton Suprunchuk <antouhou@gmail.com>
2019-05-23 15:20:59 +0300

    Add tests for core test cases

--- test/unit/transactionsFilter/testTransactionAgainstFilter.spec.js
+++ test/unit/transactionsFilter/testTransactionAgainstFilter.spec.js
@@ -210,4 +210,64 @@ describe('testTransactionAgainstFilter', () => {
 
     expect(testTransactionAgainstFilter(filter, txWIthInput)).to.be.false();
   });
+
+  it('Should pass the same test vector as dashcore implementation does', () => {
+    const txHex = '01000000010b26e9b7735eb6aabdf358bab62f9816a21ba9ebdb719d5299e88607d722c190000000008b4830450220070aca44506c5cef3a16ed519d7c3c39f8aab192c4e1c90d065f37b8a4af6141022100a8e160b856c2d43d27d8fba71e5aef6405b8643ac4cb7cb3c462aced7f14711a0141046d11fee51b0e60666d5049a9101a72741df480b96ee26488a4d3466b95c9a40ac5eeef87e10a5cd336c19a84565f80fa6c547957b7700ff4dfbdefe76036c339ffffffff021bff3d11000000001976a91404943fdd508053c75000106d3bc6e2754dbcff1988ac2f15de00000000001976a914a266436d2965547608b9e15d9032a7b9d64fa43188ac00000000';
+    const txHash = 'b4749f017444b051c44dfd2720e88f314ff94f3dd6d56d40ef65854fcd7fff6b';
+    const inputSignature = '30450220070aca44506c5cef3a16ed519d7c3c39f8aab192c4e1c90d065f37b8a4af6141022100a8e160b856c2d43d27d8fba71e5aef6405b8643ac4cb7cb3c462aced7f14711a01';
+    const inputPubKey = '046d11fee51b0e60666d5049a9101a72741df480b96ee26488a4d3466b95c9a40ac5eeef87e10a5cd336c19a84565f80fa6c547957b7700ff4dfbdefe76036c339';
+    const outputAddress = '04943fdd508053c75000106d3bc6e2754dbcff19';
+    const outputAddress2 = 'a266436d2965547608b9e15d9032a7b9d64fa431';
+    const outputIndex = '90c122d70786e899529d71dbeba91ba216982fb6ba58f3bdaab65e73b7e9260b00000000';
+    const randomHash = '00000009e784f32f62ef849763d4f45b98e07ba658647343b915ff832b110436';
+    const randomAddress = '0000006d2965547608b9e15d9032a7b9d64fa431';
+    const irrelevantOutputIndex = '90c122d70786e899529d71dbeba91ba216982fb6ba58f3bdaab65e73b7e9260b00000001';
+    const irrelevantOutputIndex2 = '000000d70786e899529d71dbeba91ba216982fb6ba58f3bdaab65e73b7e9260b00000000';
+
+    const tx = new Transaction(txHex);
+    console.log(tx.inputs[0].prevTxId.toString('hex'));
+
+    let filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(txHash, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(inputSignature, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(inputPubKey, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(outputAddress, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(outputAddress2, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(outputIndex, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.true();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(randomHash, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.false();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(randomAddress, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.false();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(irrelevantOutputIndex, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.false();
+
+    filter = BloomFilter.create(1, 0.0001, 0, BloomFilter.BLOOM_UPDATE_ALL);
+    filter.insert(Buffer.from(irrelevantOutputIndex2, 'hex'));
+    expect(testTransactionAgainstFilter(filter, tx)).to.be.false();
+  });
+
+  // TODO: test coinbase tx;
+  // TODO: test outpoint serialization
 });
