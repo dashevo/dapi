6f7f44e1460da845aebdaba4f0e607ce7abbbc61
Djavid Gabibiyan <djavidoff@gmail.com>
2019-07-16 16:05:15 +0500

    Implement pending functional test

--- test/functional/grpcServer/handlers/tx-filter-stream/subscribeToTransactionsWithProofsHandlerFactory.spec.js
+++ test/functional/grpcServer/handlers/tx-filter-stream/subscribeToTransactionsWithProofsHandlerFactory.spec.js
@@ -77,8 +77,10 @@ describe('subscribeToTransactionsWithProofsHandlerFactory', function main() {
     await coreAPI.sendToAddress(addressString, 10);
     await coreAPI.generate(10);
 
+    // Store current best block hash to cut off noise txs and merkle blocks
     ({ result: fromBlockHash } = await coreAPI.getBestBlockHash());
 
+    // Prepare historical transactions
     const filterUnspentInputs = input => input.address === addressString;
     for (let i = 0; i < 10; i++) {
       const { result: unspent } = await coreAPI.listUnspent();
@@ -415,5 +417,120 @@ describe('subscribeToTransactionsWithProofsHandlerFactory', function main() {
       .equal(lastReceivedMerkleBlock.toObject());
   });
 
-  it('should respond with only new data');
+  it('should respond with only new data', async () => {
+    const receivedTransactions = [];
+    const receivedMerkleBlocks = [];
+
+    const bloomFilterObject = bloomFilter.toObject();
+
+    // Generate one other block without matching txs
+    await coreAPI.generate(1);
+    const { result: bestBlockHash } = await coreAPI.getBestBlockHash();
+
+    // Send some transaction so it would located in mempool
+    // by the time we're going to connect (we should not receive it)
+    const { result: unspent } = await coreAPI.listUnspent();
+    const inputs = unspent.filter(input => input.address === addressString);
+
+    const transaction = new Transaction()
+      .from(inputs)
+      .to(address, 10000)
+      .change(address)
+      .sign(privateKey);
+
+    historicalTransactions.push(transaction);
+
+    await coreAPI.sendRawTransaction(transaction.serialize());
+
+    // Connect to the stream
+    const stream = await dapiClient.subscribeToTransactionsWithProofs(
+      {
+        vData: new Uint8Array(bloomFilterObject.vData),
+        nHashFuncs: bloomFilterObject.nHashFuncs,
+        nTweak: bloomFilterObject.nTweak,
+        nFlags: bloomFilterObject.nFlags,
+      },
+      {
+        fromBlockHash: Buffer.from(bestBlockHash, 'hex'),
+      },
+    );
+
+    stream.on('data', (response) => {
+      const merkleBlock = response.getRawMerkleBlock();
+      const transactions = response.getRawTransactions();
+
+      if (merkleBlock) {
+        receivedMerkleBlocks.push(
+          Buffer.from(merkleBlock).toString('hex'),
+        );
+      }
+
+      if (transactions) {
+        transactions.getTransactionsList()
+          .forEach((tx) => {
+            receivedTransactions.push(
+              new Transaction(Buffer.from(tx)),
+            );
+          });
+      }
+    });
+
+    let streamEnded = false;
+    stream.on('end', () => {
+      streamEnded = true;
+    });
+
+    let streamError;
+    stream.on('error', (e) => {
+      streamError = e;
+    });
+
+    await wait(20000);
+
+    // We should not receive tx until it is mined as we connected to late
+    expect(receivedTransactions).to.have.a.lengthOf(0);
+
+    if (streamEnded) {
+      throw new Error('Stream has ended');
+    }
+
+    if (streamError) {
+      throw streamError;
+    }
+
+    // Mine the transaction
+    await coreAPI.generate(1);
+
+    await wait(20000);
+
+    ({ result: merkleBlockStrings } = await coreAPI.getMerkleBlocks(
+      bloomFilter.toBuffer().toString('hex'),
+      bestBlockHash,
+    ));
+
+    // We should receive only one tx
+    expect(receivedTransactions).to.have.a.lengthOf(1);
+
+    const lastReceivedTransaction = receivedTransactions[receivedTransactions.length - 1];
+    const lastHistoricalTransaction = historicalTransactions[historicalTransactions.length - 1];
+
+    expect(lastReceivedTransaction.hash).to.deep.equal(lastHistoricalTransaction.hash);
+
+    const lastHistoricalMerkleBlock = new MerkleBlock(
+      Buffer.from(
+        merkleBlockStrings[merkleBlockStrings.length - 1],
+        'hex',
+      ),
+    );
+
+    const lastReceivedMerkleBlock = new MerkleBlock(
+      Buffer.from(
+        receivedMerkleBlocks[receivedMerkleBlocks.length - 1],
+        'hex',
+      ),
+    );
+
+    expect(lastHistoricalMerkleBlock.toObject()).to.deep
+      .equal(lastReceivedMerkleBlock.toObject());
+  });
 });
