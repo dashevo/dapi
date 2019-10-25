e9b25bf8f0a95d13c92a3ca84880d1d206c2a6ae
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2019-10-25 18:02:08 +0300

    Tests fix

--- test/functional/grpcServer/handlers/core/updateStateHandlerFactory.js
+++ test/functional/grpcServer/handlers/core/updateStateHandlerFactory.js
@@ -6,12 +6,17 @@ const {
   UpdateStateTransitionResponse,
 } = require('@dashevo/dapi-grpc');
 
+const {
+  PrivateKey,
+  Transaction,
+} = require('@dashevo/dashcore-lib');
+
 const DataContractStateTransition = require(
   '@dashevo/dpp/lib/dataContract/stateTransition/DataContractStateTransition',
 );
 
 const getDataContractFixture = require(
-  '@dashevo/dpp/lib/test/fixtures/getDataContractFixture',
+  '../../../../../lib/test/fixtures/getDataContractFixture',
 );
 
 describe('updateStateHandlerFactory', function main() {
@@ -21,11 +26,13 @@ describe('updateStateHandlerFactory', function main() {
   let dapiClient;
   let driveClient;
   let stateTransition;
+  let userId;
 
   beforeEach(async () => {
     const {
       driveApi,
       dapiCore,
+      dashCore,
       remove,
     } = await startDapi();
 
@@ -33,8 +40,43 @@ describe('updateStateHandlerFactory', function main() {
 
     dapiClient = dapiCore.getApi();
     driveClient = driveApi.getApi();
+    const coreAPI = dashCore.getApi();
 
-    const dataContract = getDataContractFixture();
+    const { result: addressString } = await coreAPI.getNewAddress();
+    const { result: privateKeyString } = await coreAPI.dumpPrivKey(addressString);
+
+    const privateKey = new PrivateKey(privateKeyString);
+
+    await coreAPI.generate(500);
+    await coreAPI.sendToAddress(addressString, 10);
+    await coreAPI.generate(10);
+
+    const { result: unspent } = await coreAPI.listUnspent();
+    const inputs = unspent.filter(input => input.address === addressString);
+
+    const transactionPayload = new Transaction.Payload.SubTxRegisterPayload();
+
+    const userName = 'dashUser';
+
+    transactionPayload.setUserName(userName)
+      .setPubKeyIdFromPrivateKey(privateKey)
+      .sign(privateKey);
+
+    const transaction = new Transaction({
+      type: Transaction.TYPES.TRANSACTION_SUBTX_REGISTER,
+      version: 3,
+      extraPayload: transactionPayload.toString(),
+    });
+
+    transaction.from(inputs)
+      .addFundingOutput(10000)
+      .change(addressString)
+      .fee(668)
+      .sign(privateKey);
+
+    ({ result: userId } = await coreAPI.sendrawtransaction(transaction.serialize()));
+
+    const dataContract = getDataContractFixture(userId);
 
     stateTransition = new DataContractStateTransition(dataContract);
   });
