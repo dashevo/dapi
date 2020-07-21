2006d26dab505875cefe52ffb0151773bbbbc1b9
Djavid Gabibiyan <djavidoff@gmail.com>
2020-07-21 14:58:25 +0500

    fix: unit test case

--- test/unit/grpcServer/handlers/tx-filter-stream/subscribeToTransactionsWithProofsHandlerFactory.spec.js
+++ test/unit/grpcServer/handlers/tx-filter-stream/subscribeToTransactionsWithProofsHandlerFactory.spec.js
@@ -155,76 +155,29 @@ describe('subscribeToTransactionsWithProofsHandlerFactory', () => {
     }
   });
 
-  it('should subscribe to new transactions from 0 height if both fromBlockHash and fromBlockHeight are not specified', async function it() {
-    const hash = 'someHash';
-    const hashHex = Buffer.from('someHash').toString('hex');
+  it('should respond with error if if both fromBlockHash and fromBlockHeight are not specified', async () => {
+    const bloomFilterMessage = new BloomFilter();
 
-    coreAPIMock.getBlock.resolves({ height: 1 });
-    coreAPIMock.getBestBlockHeight.resolves(10);
-    coreAPIMock.getBlockHash.resolves(hashHex);
+    bloomFilterMessage.setVData(new Uint8Array());
+    bloomFilterMessage.setNTweak(1000);
+    bloomFilterMessage.setNFlags(100);
+    bloomFilterMessage.setNHashFuncs(10);
 
-    call.request.setCount(10);
+    const request = new TransactionsWithProofsRequest();
 
-    const writableStub = this.sinon.stub(AcknowledgingWritable.prototype, 'write');
+    request.setBloomFilter(bloomFilterMessage);
 
-    coreAPIMock.getBlock.resolves({ height: 1 });
-    coreAPIMock.getBestBlockHeight.resolves(10);
+    call.request = request;
 
-    historicalTxData.push({
-      merkleBlock: {
-        toBuffer: () => Buffer.from(hash),
-        header: {
-          hash,
-        },
-      },
-      transactions: [
-        {
-          toBuffer: () => Buffer.from(
-            'edefad1c70ee6736a0a0c2f9be7f22cfcf77ae2c120704a98cdc9aebdab7ffc5', 'hex',
-          ),
-        },
-      ],
-    });
-
-    await subscribeToTransactionsWithProofsHandler(call);
-
-    const filter = new CoreBloomFilter({
-      vData: new Uint8Array([]),
-      nTweak: 1000,
-      nFlags: 100,
-      nHashFuncs: 10,
-    });
-
-    expect(getHistoricalTransactionsIteratorMock).to.have.been
-      .calledOnceWith(
-        filter,
-        hashHex,
-        10,
-      );
-
-    expect(subscribeToNewTransactionsMock).to.not.have.been.called();
-    expect(writableStub).to.have.been.calledTwice();
-
-    const firstResponse = new TransactionsWithProofsResponse();
-    const rawTransactions = new RawTransactions();
-    rawTransactions.setTransactionsList(
-      historicalTxData[0].transactions.map(tx => tx.toBuffer()),
-    );
-    firstResponse.setRawTransactions(rawTransactions);
-
-    const secondResponse = new TransactionsWithProofsResponse();
-    secondResponse.setRawMerkleBlock(historicalTxData[0].merkleBlock.toBuffer());
-
-    expect(writableStub.getCall(0).args).to.deep.equal(
-      [firstResponse],
-    );
-
-    expect(writableStub.getCall(1).args).to.deep.equal(
-      [secondResponse],
-    );
-
-    expect(coreAPIMock.getBlockHash).to.be.calledOnceWith(0);
-    expect(coreAPIMock.getBestBlockHeight).to.be.calledThrice();
+    try {
+      await subscribeToTransactionsWithProofsHandler(call);
+      expect.fail('Error was not thrown');
+    } catch (e) {
+      expect(e).to.be.an.instanceOf(InvalidArgumentGrpcError);
+      expect(e.getMessage()).to.equal('minimum value for `fromBlockHeight` is 1');
+      expect(call.write).to.not.have.been.called();
+      expect(call.end).to.not.have.been.called();
+    }
   });
 
   it('should respond with error if fromBlockHeight exceeded blockchain length', async () => {
