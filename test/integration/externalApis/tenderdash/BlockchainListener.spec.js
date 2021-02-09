d09d3befa43350dc1ac7460eef5bd7adb0e92f3e
Anton Suprunchuk <antouhou@gmail.com>
2021-02-09 19:37:33 +0300

    fix tests

--- test/integration/externalApis/tenderdash/BlockchainListener.spec.js
+++ test/integration/externalApis/tenderdash/BlockchainListener.spec.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const crypto = require('crypto');
 const BlockchainListener = require('../../../../lib/externalApis/tenderdash/BlockchainListener');
 const TransactionWaitPeriodExceededError = require('../../../../lib/errors/TransactionWaitPeriodExceededError');
 
@@ -7,6 +8,10 @@ describe('BlockchainListener', () => {
   let wsClientMock;
   let blockchainListener;
   let txDataMock;
+  const base64tx = 'aaaa';
+  let txHash;
+  let blockWithTxMock;
+  let emptyBlockMock;
 
   beforeEach(function beforeEach() {
     ({ sinon } = this);
@@ -15,10 +20,22 @@ describe('BlockchainListener', () => {
     blockchainListener = new BlockchainListener(wsClientMock);
     blockchainListener.start();
 
+    txHash = crypto.createHash('sha256')
+      .update(Buffer.from(base64tx, 'base64'))
+      .digest()
+      .toString('hex');
+
     txDataMock = {
       events: {
-        'tx.hash': ['123'],
+        'tx.hash': [txHash],
       },
+    };
+
+    blockWithTxMock = {
+      data: { value: { block: { data: { txs: [base64tx] } } } },
+    };
+    emptyBlockMock = {
+      data: { value: { block: { data: { txs: [] } } } },
     };
 
     sinon.spy(blockchainListener, 'on');
@@ -40,47 +57,58 @@ describe('BlockchainListener', () => {
 
   describe('.getTransactionEventName', () => {
     it('should return transaction event name', () => {
-      const eventName = BlockchainListener.getTransactionEventName('123');
-      expect(eventName).to.be.equal('transaction:123');
+      const eventName = BlockchainListener.getTransactionEventName(txHash);
+      expect(eventName).to.be.equal(`transaction:${txHash}`);
     });
   });
 
   describe('#waitForTransaction', () => {
     it('should remove listener after transaction resolves', async () => {
-      const eventName = BlockchainListener.getTransactionEventName('123');
-      const txPromise = blockchainListener.waitForTransaction('123', 2000);
+      const txPromise = blockchainListener.waitForTransactionToBeProvable(txHash, 2000);
 
-      expect(blockchainListener.listenerCount(eventName)).to.be.equal(1);
+      // check that we attached events correctly
+      const events = blockchainListener.eventNames();
+      // transaction result, transaction in block an block events
+      expect(events.length).to.be.equal(3);
+      events.forEach((eventName) => {
+        expect(blockchainListener.listenerCount(eventName)).to.be.equal(1);
+      });
 
       setTimeout(() => {
         wsClientMock.emit(BlockchainListener.TX_QUERY, Object.assign({}, txDataMock));
+        wsClientMock.emit(BlockchainListener.NEW_BLOCK_QUERY, Object.assign({}, blockWithTxMock));
+        wsClientMock.emit(BlockchainListener.NEW_BLOCK_QUERY, Object.assign({}, emptyBlockMock));
       }, 10);
 
       const txData = await txPromise;
 
       // Check that event listener was properly attached
-      expect(blockchainListener.on).to.be.calledOnce();
-      // Check that transaction data was emitted
-      expect(blockchainListener.emit).to.be.calledOnce();
+      expect(blockchainListener.on).to.be.calledThrice();
       // Check that the event listener was properly removed
-      expect(blockchainListener.off).to.be.calledOnce();
-      expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      expect(blockchainListener.off).to.be.calledTwice();
+      events.forEach((eventName) => {
+        expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      });
 
       expect(txData).to.be.deep.equal(txDataMock);
     });
 
     it('should not emit transaction event if event data has no transaction', async () => {
-      const eventName = BlockchainListener.getTransactionEventName('123');
-      txDataMock = {};
-
+      let events;
       let error;
       try {
-        const txPromise = blockchainListener.waitForTransaction('123', 1000);
+        const txPromise = blockchainListener.waitForTransactionToBeProvable(txHash, 1000);
 
-        expect(blockchainListener.listenerCount(eventName)).to.be.equal(1);
+        // check that we attached events correctly
+        events = blockchainListener.eventNames();
+        // transaction result, transaction in block an block events
+        expect(events.length).to.be.equal(3);
+        events.forEach((eventName) => {
+          expect(blockchainListener.listenerCount(eventName)).to.be.equal(1);
+        });
 
         setTimeout(() => {
-          wsClientMock.emit(BlockchainListener.TX_QUERY, Object.assign({}, txDataMock));
+          wsClientMock.emit(BlockchainListener.NEW_BLOCK_QUERY, Object.assign({}, blockWithTxMock));
         }, 10);
 
         await txPromise;
@@ -90,64 +118,50 @@ describe('BlockchainListener', () => {
 
       // Check that the error is correct
       expect(error).to.be.instanceOf(TransactionWaitPeriodExceededError);
-      expect(error.message).to.be.equal('Transaction waiting period for 123 exceeded');
-      expect(error.getTransactionHash()).to.be.equal('123');
+      expect(error.message).to.be.equal(`Transaction waiting period for ${txHash} exceeded`);
+      expect(error.getTransactionHash()).to.be.equal(txHash);
 
       // Check that event listener was properly attached
-      expect(blockchainListener.on).to.be.calledOnce();
-      // Check that event listener was properly removed
-      expect(blockchainListener.off).to.be.calledOnce();
-      expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      expect(blockchainListener.on).to.be.calledThrice();
+      events.forEach((eventName) => {
+        expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      });
       // Check that no transaction data was emitted
-      expect(blockchainListener.emit).to.not.be.called();
+      expect(blockchainListener.emit).to.be.calledTwice();
     });
 
     it('should remove listener after timeout has been exceeded', async () => {
-      const eventName = BlockchainListener.getTransactionEventName('123');
+      let events;
       let error;
       try {
-        await blockchainListener.waitForTransaction('123', 100);
+        const txPromise = blockchainListener.waitForTransactionToBeProvable(txHash, 100);
+
+        events = blockchainListener.eventNames();
+        expect(events.length).to.be.equal(3);
+        events.forEach((eventName) => {
+          expect(blockchainListener.listenerCount(eventName)).to.be.equal(1);
+        });
+
+        await txPromise;
       } catch (e) {
         error = e;
       }
 
       // Check that the error is correct
       expect(error).to.be.instanceOf(TransactionWaitPeriodExceededError);
-      expect(error.message).to.be.equal('Transaction waiting period for 123 exceeded');
-      expect(error.getTransactionHash()).to.be.equal('123');
+      expect(error.message).to.be.equal(`Transaction waiting period for ${txHash} exceeded`);
+      expect(error.getTransactionHash()).to.be.equal(txHash);
 
       // Check that event listener was properly attached
-      expect(blockchainListener.on).to.be.calledOnce();
+      expect(blockchainListener.on).to.be.calledThrice();
       // Check that event listener was properly removed
-      expect(blockchainListener.off).to.be.calledOnce();
-      expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      expect(blockchainListener.off).to.be.calledThrice();
+
+      events.forEach((eventName) => {
+        expect(blockchainListener.listenerCount(eventName)).to.be.equal(0);
+      });
       // Check that no transaction data was emitted
       expect(blockchainListener.emit).to.not.be.called();
-    });
-  });
-
-  describe('#waitForBlocks', () => {
-    it('should wait for n blocks and remove listeners afterwards', async () => {
-      const newBlockEvent = BlockchainListener.events.NEW_BLOCK;
-      const blockPromise = blockchainListener.waitForBlocks(2);
-
-      expect(blockchainListener.listenerCount(newBlockEvent)).to.be.equal(1);
-
-      setTimeout(() => {
-        wsClientMock.emit(BlockchainListener.NEW_BLOCK_QUERY, Object.assign({}, txDataMock));
-      }, 10);
-      setTimeout(() => {
-        wsClientMock.emit(BlockchainListener.NEW_BLOCK_QUERY, Object.assign({}, txDataMock));
-      }, 10);
-
-      await blockPromise;
-
-      // Check that event listener was properly attached
-      expect(blockchainListener.on).to.be.calledOnce();
-      // Check that transaction data was emitted
-      expect(blockchainListener.emit).to.be.calledTwice();
-      // Check that the event listener was properly removed
-      expect(blockchainListener.listenerCount(newBlockEvent)).to.be.equal(0);
     });
   });
 });
