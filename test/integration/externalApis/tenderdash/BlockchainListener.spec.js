7bf04fe933a957dca40985181ec492f29e7bb5f6
Anton Suprunchuk <antouhou@gmail.com>
2021-02-16 18:21:27 +0300

    remove unused variables

--- test/integration/externalApis/tenderdash/BlockchainListener.spec.js
+++ test/integration/externalApis/tenderdash/BlockchainListener.spec.js
@@ -1,17 +1,10 @@
 const EventEmitter = require('events');
-const crypto = require('crypto');
 const BlockchainListener = require('../../../../lib/externalApis/tenderdash/blockchainListener/BlockchainListener');
-const TransactionWaitPeriodExceededError = require('../../../../lib/errors/TransactionWaitPeriodExceededError');
 
 describe('BlockchainListener', () => {
   let sinon;
   let wsClientMock;
   let blockchainListener;
-  let txDataMock;
-  const base64tx = 'aaaa';
-  let txHash;
-  let blockWithTxMock;
-  let emptyBlockMock;
 
   beforeEach(function beforeEach() {
     ({ sinon } = this);
@@ -19,24 +12,6 @@ describe('BlockchainListener', () => {
     wsClientMock.subscribe = sinon.stub();
     blockchainListener = new BlockchainListener(wsClientMock);
     blockchainListener.start();
-
-    txHash = crypto.createHash('sha256')
-      .update(Buffer.from(base64tx, 'base64'))
-      .digest()
-      .toString('hex');
-
-    txDataMock = {
-      events: {
-        'tx.hash': [txHash],
-      },
-    };
-
-    blockWithTxMock = {
-      data: { value: { block: { data: { txs: [base64tx] } } } },
-    };
-    emptyBlockMock = {
-      data: { value: { block: { data: { txs: [] } } } },
-    };
 
     sinon.spy(blockchainListener, 'on');
     sinon.spy(blockchainListener, 'off');
